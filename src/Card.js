109a74c7f6ab0f3170dfc9843bf901d25ec7fb88
Loredana <puiu.eloredana@gmail.com>
2022-02-03 17:03:07 +0200

    robo search

---
+++ src/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/* destructuring  */
+/* if we dont want to use destructuring we write  const Card = (props) => */
+/* and use propertis by  <h2>{props.name}</h2>*/
+const Card = ( { name, email, id}) => {
+    return (
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-s'>
+            <img alt='Robots' src={`https://robohash.org/${id}?200x200`} />
+            <div>
+            {/* this is js expresion and we need to wrap it in {} */} 
+                <h2>{name}</h2>
+                <p>{email}</p> 
+            </div>
+        </div>
+    );
+}
+
+
+
+export default Card
+//need to return just one element
